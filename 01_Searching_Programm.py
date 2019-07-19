0235df2fe3064095bfa147fa93cab477d3c79b05
Archie <archiebolt@users.noreply.github.com>
2019-07-19 21:58:47 +0200

    Update 01_Searching_Programm.py

--- 01_Searching_Programm.py
+++ 01_Searching_Programm.py
@@ -1,10 +1,20 @@
+#  First you need to put text into a "text" variable
+#  Then by turning this code on, it will ask what you searching for
+#  if you, for example, type the letter "a" - it will close all a's into () in the text.
+
+
 text = """
-Für einen Blog benötigen Sie eine Plattform, die bestimmte Voraussetzungen erfüllt. Einige Anbieter verfügen über entsprechend eingerichtete Server
-und stellen diese zur Verfügung. Hier bekommen Sie eine eigene Internetadresse für Ihren Blog zugeteilt. Über den Administrationsbereich richten Sie
-ihn Ihren Vorstellungen entsprechend ein und füllen ihn mit Inhalten. In verschiedenen Details unterscheiden sich die Anbieter jedoch.
-Diese gilt es zunächst zu vergleichen, um von Anfang an die richtige Entscheidung zu treffen: Stellt sich später heraus, dass Sie mit der Wahl
-unzufrieden sind, weil der Blog nicht die gewünschten Möglichkeiten bietet, waren Ihre Bemühungen umsonst. Sie müssen auf einer geeigneteren Plattform von vorne beginnen.
-"""  # You can paste pretty much any text you want
+The Elder Scrolls V: Skyrim is an action role-playing game, playable from either a first or third-person perspective.
+The player may freely roam over the land of Skyrim which is an open world environment consisting of wilderness expanses,
+dungeons, cities, towns, fortresses, and villages. Players may navigate the game world more quickly by riding horses or
+by utilizing a fast-travel system which allows them to warp to previously discovered locations. The game's main
+quest can be completed or ignored at the player's preference after the first stage of the quest is finished. However,
+some quests rely on the main storyline being at least partially completed. Non-player characters (NPCs) populate
+the world and can be interacted with in a number of ways: the player may engage them in conversation, marry an eligible
+NPC, kill them or engage in a nonlethal "brawl". As in previous The Elder Scrolls games, killing certain NPCs can make
+some quests or items unobtainable. Some NPCs cannot be killed due to their importance in later storylines. If witnessed,
+crimes like murder and theft accrue the player a bounty which is tracked independently in each of Skyrim's nine holds.
+"""  # You can paste pretty much any text you want / Right now this is just some random text of the elder scrolls game
 print(text)  # Just shows the text to user
 final_text_words = ''
 final_text_characters = ''
